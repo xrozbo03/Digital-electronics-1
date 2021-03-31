6a1c98d43093d116d804399c64bcff7391dabe87
unknown <xrozbo03@vutbr.cz>
2021-03-31 17:11:01 +0200

    [LAB] add 08-traffic_lights

--- Labs/08-traffic_lights/traffic/traffic.srcs/sources_1/new/tlc.vhd
+++ Labs/08-traffic_lights/traffic/traffic.srcs/sources_1/new/tlc.vhd
@@ -1,33 +1,89 @@
-# Lab 8: Traffic light controller
+------------------------------------------------------------------------
+--
+-- Traffic light controller using FSM.
+-- Nexys A7-50T, Vivado v2020.1.1, EDA Playground
+--
+-- Copyright (c) 2020-Present Tomas Fryza
+-- Dept. of Radio Electronics, Brno University of Technology, Czechia
+-- This work is licensed under the terms of the MIT license.
+--
+-- This code is inspired by:
+-- [1] LBEbooks, Lesson 92 - Example 62: Traffic Light Controller
+--     https://www.youtube.com/watch?v=6_Rotnw1hFM
+-- [2] David Williams, Implementing a Finite State Machine in VHDL
+--     https://www.allaboutcircuits.com/technical-articles/implementing-a-finite-state-machine-in-vhdl/
+-- [3] VHDLwhiz, One-process vs two-process vs three-process state machine
+--     https://vhdlwhiz.com/n-process-state-machine/
+--
+------------------------------------------------------------------------
 
-## 1. Preparation
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
 
-### Completed state table
+------------------------------------------------------------------------
+-- Entity declaration for traffic light controller
+------------------------------------------------------------------------
+entity tlc is
+    port(
+        clk     : in  std_logic;
+        reset   : in  std_logic;
+        -- Traffic lights (RGB LEDs) for two directions
+        south_o : out std_logic_vector(3 - 1 downto 0);
+        west_o  : out std_logic_vector(3 - 1 downto 0)
+    );
+end entity tlc;
 
-| **Input P** | `0` | `0` | `1` | `1` | `0` | `1` | `0` | `1` | `1` | `1` | `1` | `0` | `0` | `1` | `1` | `1` |
-| :-- | :-: | :-: | :-: | :-: | :-: | :-: | :-: | :-: | :-: | :-: | :-: | :-: | :-: | :-: | :-: | :-: |
-| **Clock** | ![rising](Images/eq_uparrow.png) | ![rising](Images/eq_uparrow.png) | ![rising](Images/eq_uparrow.png) | ![rising](Images/eq_uparrow.png) | ![rising](Images/eq_uparrow.png) | ![rising](Images/eq_uparrow.png) | ![rising](Images/eq_uparrow.png) | ![rising](Images/eq_uparrow.png) | ![rising](Images/eq_uparrow.png) | ![rising](Images/eq_uparrow.png) | ![rising](Images/eq_uparrow.png) | ![rising](Images/eq_uparrow.png) | ![rising](Images/eq_uparrow.png) | ![rising](Images/eq_uparrow.png) | ![rising](Images/eq_uparrow.png) | ![rising](Images/eq_uparrow.png) |
-| **State** | A | A | B | C | C | D | A | B | C | D | B | B | B | C | D | B |
-| **Output R** | `0` | 0 | 0 | 0 | 0 | 1 | 0 | 0 | 0 | 1 | 0 | 0 | 0 | 0 | 1 | 0 |
+------------------------------------------------------------------------
+-- Architecture declaration for traffic light controller
+------------------------------------------------------------------------
+architecture Behavioral of tlc is
 
-### Figure with connection of RGB LEDs on Nexys A7 board and completed table with color settings
+    -- Define the states
+    type t_state is (STOP1,
+                     WEST_GO,
+                     WEST_WAIT,
+                     STOP2,
+                     SOUTH_GO,
+                     SOUTH_WAIT);
+    -- Define the signal that uses different states
+    signal s_state  : t_state;
 
-![RGB LEDs](Images/RGBLeds.png)
+    -- Internal clock enable
+    signal s_en     : std_logic;
+    -- Local delay counter
+    signal   s_cnt  : unsigned(5 - 1 downto 0);
 
-| **RGB LED** | **Artix-7 pin names** | **Red** | **Yellow** | **Green** |
-| :-: | :-: | :-: | :-: | :-: |
-| LD16 | N15, M16, R12 | `1,0,0` | `1,1,0` |  `0,1,0`  |
-| LD17 | N16, R11, G14 | `1,0,0` | `1,1,0` | `0,1,0` |
+    -- Specific values for local counter
+    constant c_DELAY_4SEC : unsigned(5 - 1 downto 0) := b"1_0000";
+    constant c_DELAY_2SEC : unsigned(5 - 1 downto 0) := b"0_1000";
+    constant c_DELAY_1SEC : unsigned(5 - 1 downto 0) := b"0_0100";
+    constant c_ZERO       : unsigned(5 - 1 downto 0) := b"0_0000";
 
-## 2. Traffic light controller
+begin
 
-### State diagram
+    --------------------------------------------------------------------
+    -- Instance (copy) of clock_enable entity generates an enable pulse
+    -- every 250 ms (4 Hz). Remember that the frequency of the clock 
+    -- signal is 100 MHz.
+    
+    -- JUST FOR SHORTER/FASTER SIMULATION
+    s_en <= '1';
+--    clk_en0 : entity work.clock_enable
+--        generic map(
+--            g_MAX =>        -- g_MAX = 250 ms / (1/100 MHz)
+--        )
+--        port map(
+--            clk   => clk,
+--            reset => reset,
+--            ce_o  => s_en
+--        );
 
-![TLC State Diagram](Images/TLCStateDiagram.png)
-
-### Listing of VHDL code of sequential process `p_traffic_fsm`
-
-```vhdl
+    --------------------------------------------------------------------
+    -- p_traffic_fsm:
+    -- The sequential process with synchronous reset and clock_enable 
+    -- entirely controls the s_state signal by CASE statement.
+    --------------------------------------------------------------------
     p_traffic_fsm : process(clk)
     begin
         if rising_edge(clk) then
@@ -119,11 +175,13 @@
             end if; -- Synchronous reset
         end if; -- Rising edge
     end process p_traffic_fsm;
-```
 
-### Listing of VHDL code of combinatorial process `p_output_fsm`
-
-```vhdl
+    --------------------------------------------------------------------
+    -- p_output_fsm:
+    -- The combinatorial process is sensitive to state changes, and sets
+    -- the output signals accordingly. This is an example of a Moore 
+    -- state machine because the output is set based on the active state.
+    --------------------------------------------------------------------
     p_output_fsm : process(s_state)
     begin
         case s_state is
@@ -150,24 +208,5 @@
                 west_o  <= "100";   -- Red
         end case;
     end process p_output_fsm;
-```
 
-### Screenshot(s) of the simulation
-
-![waveforms](Images/waveforms.png)
-
-## 3. Smart controller
-
-### State table
-
-
-
-### State diagram
-
-![TLSC State Diagram](Images/TLSCStateDiagram.png)
-
-### Listing of VHDL code of sequential process `p_smart_traffic_fsm`
-
-```vhdl
-
-```
+end architecture Behavioral;
